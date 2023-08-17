760a6aae33c0fa8854a0f865731b3d3f7ca57004
tombom <inoulenkon@gmail.com>
2023-08-17 14:00:36 +0530

    Update model post to reflect update_posts_counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+  after_save :update_posts_counter
 
   def update_posts_counter
     author.increment!(:posts_counter)
