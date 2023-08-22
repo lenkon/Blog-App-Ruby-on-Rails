3da269062ade32fdbfd43bac93c29ed87823dc08
tombom <inoulenkon@gmail.com>
2023-08-22 12:20:40 +0530

    Add show user action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-    def index
-      @users = User.all
-    end
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user_id = params[:id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users
+  
+  root 'users#index'
+  resources :users, only: [:index, :show]
 end
