5dd19d8a138d2e676a2641843fe3e67c9b2e5e20
tombom <inoulenkon@gmail.com>
2023-08-22 13:24:17 +0530

    Add posts show action, view and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index]
+    resources :posts, only: [:index, :show]
   end
 end
