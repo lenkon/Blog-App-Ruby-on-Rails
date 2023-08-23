70efc51a98c965eea0bc562dd21d88f95135ad64
tombom <inoulenkon@gmail.com>
2023-08-23 17:06:33 +0530

    Setup test for controllers, add test for rendered template and response status for users

---
+++ spec/controllers/users_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: %w[request feature] do
+  before :each do
+    @user = User.new(
+      name: 'Max',
+      bio: 'Full-stack developer',
+      photo: 'http://localhost/123',
+      posts_counter: 0
+    )
+    @user.save
+  end
+
+  it 'renders index template with status OK' do
+    get '/users/'
+    expect(response).to render_template('index')
+    expect(response.status).to eq(200)
+  end
+
+  it 'renders show template with status OK' do
+    get '/users/3'
+    expect(response).to render_template('show')
+    expect(response.status).to eq(200)
+  end
+end
