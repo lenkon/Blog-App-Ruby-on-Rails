bbe918921373b02615af34d98fe58ddc2e3f2ca0
tombom <inoulenkon@gmail.com>
2023-08-23 17:39:09 +0530

    Add test for rendered template and response status for posts

---
+++ spec/controllers/posts_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: %w[request feature] do
+  before :each do
+    @user = User.new(
+      name: 'Max',
+      bio: 'Full-stack developer',
+      photo: 'http://localhost/123',
+      posts_counter: 0
+    )
+    @user.save
+  end
+
+  it 'renders index template with status OK for post' do
+    get '/users/1/posts'
+    expect(response).to render_template('index')
+    expect(response.status).to eq(200)
+  end
+
+  it 'renders show template with status OK for a post' do
+    get '/users/1/posts/3'
+    expect(response).to render_template('show')
+    expect(response.status).to eq(200)
+  end
+end
