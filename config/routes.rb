e7aede7d2405e23116fc8042bb974068fa5c646d
tombom <inoulenkon@gmail.com>
2023-08-25 12:24:46 +0530

    Add form to create a post for current user

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new]
+    resources :posts, only: [:index, :new, :create, :show]
   end
 end
