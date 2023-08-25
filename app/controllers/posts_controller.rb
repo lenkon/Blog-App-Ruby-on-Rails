eb2faddaee0350012c13b4bd13db9b987cfe54de
tombom <inoulenkon@gmail.com>
2023-08-25 12:13:37 +0530

    Add form to create a new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,11 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    new_post = Post.new
+    respond_to do |f|
+      f.html { render :new, locals: { new_post: } }
+    end
+  end
 end
